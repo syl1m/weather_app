460708862c33daf1adb4ecfe43d7de8a960fa081
syl1m <shengyaulim@gmail.com>
2023-11-17 00:57:05 -0600

    Dynamically display data on webpage with icons

--- src/displayData.js
+++ src/displayData.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 function createCurrentDataUI(data) {
   // Create basic layout divs
   const currentWeatherDiv = document.querySelector(".current-weather");
@@ -19,41 +20,91 @@ function createCurrentDataUI(data) {
   const condition = document.createElement("div");
   const location = document.createElement("div");
   const lastUpdated = document.createElement("div");
-  const currentTemp = document.createElement("div");
-  const conditionIcon = document.createElement("div");
+  const currentTempC = document.createElement("div");
+  const currentTempF = document.createElement("div");
+  const conditionIcon = document.createElement("img");
 
   condition.textContent = `${data.currentData.condition.text}`;
   location.textContent = `${data.locationData.name}, ${data.locationData.region}, ${data.locationData.country}`;
   lastUpdated.textContent = `${data.currentData.last_updated_date_formatted}`;
-  currentTemp.textContent = `${data.currentData.temp_c} degrees C`;
-  conditionIcon.textContent = `${data.currentData.condition.icon}`;
+  currentTempC.textContent = `${data.currentData.temp_c} °C`;
+  currentTempF.textContent = `${data.currentData.temp_f} °F`;
+  conditionIcon.src = `https:${data.currentData.condition.icon}`;
+
+  currentTempC.classList.add("metric");
+  currentTempF.classList.add("imperial");
 
   leftDiv.appendChild(condition);
   leftDiv.appendChild(location);
   leftDiv.appendChild(lastUpdated);
-  leftDiv.appendChild(currentTemp);
+  leftDiv.appendChild(currentTempC);
+  leftDiv.appendChild(currentTempF);
   leftDiv.appendChild(conditionIcon);
 
   //  Right side data
-  const feelsLike = document.createElement("div");
+  const feelsLikeC = document.createElement("div");
+  const feelsLikeF = document.createElement("div");
   const humidity = document.createElement("div");
   const rainChance = document.createElement("div");
-  const windSpeed = document.createElement("div");
+  const windSpeedKph = document.createElement("div");
+  const windSpeedMph = document.createElement("div");
 
-  feelsLike.textContent = `${data.currentData.feelslike_c} deg C`;
+  feelsLikeC.textContent = `${data.currentData.feelslike_c} °C`;
+  feelsLikeF.textContent = `${data.currentData.feelslike_f} °F`;
   humidity.textContent = `${data.currentData.humidity} %`;
-  rainChance.textContent = `${data.dailyForecastData.rainChance} %`;
-  windSpeed.textContent = `${data.currentData.wind_kph} km/h`;
+  rainChance.textContent = `${data.dailyForecastData[0].rainChance} %`;
+  windSpeedKph.textContent = `${data.currentData.wind_kph} kph`;
+  windSpeedMph.textContent = `${data.currentData.wind_mph} mph`;
 
-  rightDiv.appendChild(feelsLike);
+  feelsLikeC.classList.add("metric");
+  feelsLikeF.classList.add("imperial");
+  windSpeedKph.classList.add("metric");
+  windSpeedMph.classList.add("imperial");
+
+  rightDiv.appendChild(feelsLikeC);
+  rightDiv.appendChild(feelsLikeF);
   rightDiv.appendChild(humidity);
   rightDiv.appendChild(rainChance);
-  rightDiv.appendChild(windSpeed);
+  rightDiv.appendChild(windSpeedKph);
+  rightDiv.appendChild(windSpeedMph);
 }
 
 function createForecastDataUI(data) {
   const forecast = document.querySelector(".forecast");
   forecast.textContent = "";
+
+  for (let i = 0; i < data.dailyForecastData.length; i++) {
+    const dayDiv = document.createElement("div");
+    const weekDay = document.createElement("div");
+    const conditionIcon = document.createElement("img");
+    const condition = document.createElement("div");
+    const minTempC = document.createElement("div");
+    const minTempF = document.createElement("div");
+    const maxTempC = document.createElement("div");
+    const maxTempF = document.createElement("div");
+
+    weekDay.textContent = `${data.dailyForecastData[i].weekday}`;
+    conditionIcon.src = `https:${data.dailyForecastData[i].condition.icon}`;
+    condition.textContent = `${data.dailyForecastData[i].condition.text}`;
+    minTempC.textContent = `${data.dailyForecastData[i].mintemp_c} °C`;
+    minTempF.textContent = `${data.dailyForecastData[i].mintemp_f} °F`;
+    maxTempC.textContent = `${data.dailyForecastData[i].maxtemp_c} °C`;
+    maxTempF.textContent = `${data.dailyForecastData[i].maxtemp_f} °F`;
+
+    minTempC.classList.add("metric");
+    maxTempC.classList.add("metric");
+    minTempF.classList.add("imperial");
+    maxTempF.classList.add("imperial");
+
+    dayDiv.appendChild(weekDay);
+    dayDiv.appendChild(conditionIcon);
+    dayDiv.appendChild(condition);
+    dayDiv.appendChild(minTempC);
+    dayDiv.appendChild(minTempF);
+    dayDiv.appendChild(maxTempC);
+    dayDiv.appendChild(maxTempF);
+    forecast.appendChild(dayDiv);
+  }
 }
 
 export default function displayData(data) {
